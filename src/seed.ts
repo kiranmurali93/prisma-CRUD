29a8a8b527e9f5174675287b9691c1a4903f15b8
kiranmurali <kiranmurali93@gmail.com>
2021-09-19 14:50:14 +0530

    partOne update

--- src/seed.ts
+++ src/seed.ts
@@ -5,9 +5,13 @@ const prisma = new PrismaClient()
 
 // A `main` function so that we can use async/await
 async function main() {
-  await prisma.user.deleteMany({}) //deleting all elements
-
-  const user = await prisma.user.create({
+  //deleting all elements
+  await prisma.user.deleteMany({})
+  await prisma.courseEnrollment.deleteMany({})
+  await prisma.test.deleteMany({})
+  await prisma.course.deleteMany({})
+  
+  const testUser = await prisma.user.create({
     data:{
       email:'test@test.com',
       firstName: 'test',
@@ -18,7 +22,7 @@ async function main() {
       }
     }
   })
-  console.log(user);
+  console.log(testUser);
   
   const weekFromNow = add(new Date(), { days: 7 })
   const twoWeekFromNow = add(new Date(), { days: 14 })
@@ -43,11 +47,152 @@ async function main() {
             name: 'Final exam',
           },
         ],
+      },
+      members: {
+        create: {
+          role: 'TEACHER',
+          user: {
+            connect: {
+              email: testUser.email,
+            },
+          },
+        },
+      },
+    },
+    include: {
+      tests: true,
+      members: {
+        include: { user: true}
       }
-    }
+    },
   })
   console.log(course);
   
+  //
+  const shakuntala = await prisma.user.create({
+    data: {
+      email: 'devi@prisma.io',
+      firstName: 'Shakuntala',
+      lastName: 'Devi',
+      social: {
+        facebook : 'testFacebook',
+        twitter : 'testtwitter'
+        },
+        course: {
+          create: {
+            role: 'STUDENT',
+            course: {
+              connect : {
+                id: course.id
+              },
+            }
+          },
+        }
+    },
+  })
+
+  const david = await prisma.user.create({
+    data: {
+      email: 'david@prisma.io',
+      firstName: 'David',
+      lastName: 'Deutsch',
+      social: {
+        facebook : 'testFacebook',
+        twitter : 'testtwitter'
+      },
+      course: {
+        create: {
+          role: 'STUDENT',
+          course: {
+            connect: {
+              id: course.id
+            },
+          }
+        }
+      }
+    },
+  })
+
+  const testResultsDavid = [650, 900, 950]
+  const testResultsShakuntala = [800, 950, 910]
+
+  let counter = 0
+  for (const test of course.tests) {
+    await prisma.testResult.create({
+      data: {
+        gradedBy: {
+          connect: { email: testUser.email },
+        },
+        student: {
+          connect: { email: shakuntala.email },
+        },
+        test: {
+          connect: { id: test.id },
+        },
+        result: testResultsShakuntala[counter],
+      },
+    })
+
+    await prisma.testResult.create({
+      data: {
+        gradedBy: {
+          connect: { email: testUser.email },
+        },
+        student: {
+          connect: { email: david.email },
+        },
+        test: {
+          connect: { id: test.id },
+        },
+        result: testResultsDavid[counter],
+      },
+    })
+
+    // Get aggregates for each test
+    const results = await prisma.testResult.aggregate({
+      where: {
+        testId: test.id,
+      },
+      avg: { result: true },
+      max: { result: true },
+      min: { result: true },
+      count: true,
+    })
+    console.log(`test: ${test.name} (id: ${test.id})`, results)
+
+    counter++
+  }
+
+  // Get aggregates for David
+  const davidAggregates = await prisma.testResult.aggregate({
+    where: {
+      student: { email: david.email },
+    },
+    avg: { result: true },
+    max: { result: true },
+    min: { result: true },
+    count: true,
+  })
+  console.log(`David's results (email: ${david.email})`, davidAggregates)
+
+  // Get aggregates for Shakuntala
+  const shakuntalaAggregates = await prisma.testResult.aggregate({
+    where: {
+      student: { email: shakuntala.email },
+    },
+    avg: { result: true },
+    max: { result: true },
+    min: { result: true },
+    count: true,
+  })
+  console.log(
+    `Shakuntala's results (email: ${shakuntala.email})`,
+    shakuntalaAggregates,
+  )
+  //
+
+
+
 }
 
 main()
